c589eda9e2f6a1550d45269e12b2b801033e521a
Nitish631 <nitishrajbanshi61@gmail.com>
2025-12-09 02:40:38 +0545

    Nearly completed

--- lib/main.py
+++ lib/main.py
@@ -1,10 +1,39 @@
 import speech_recognition as sr
 import pyttsx3
 import webbrowser
-import time
+from datetime import datetime ,timedelta
+from typing import Dict ,Any
+import requests
+from secret import API_KEY
+
+
+
+def handle_news():
+    API_KEY: str = API_KEY
+    URL: str = "https://newsdata.io/api/1/latest"
+
+    params: Dict[str, str] = {
+        "apikey": API_KEY,
+        "q": "technology",
+        "language": "en"
+    }
+
+    response: requests.Response = requests.get(URL, params=params, timeout=10)
+
+    if response.status_code == 200:
+        data: Dict[str, Any] = response.json()
+        
+        # Extract all titles
+        news_titles: str = "\n".join(article.get("title", "") for article in data.get("results", []))
+        
+        speak(news_titles)
+    else:
+        print("Error:", response.status_code, response.text)
 
 r=sr.Recognizer()
 KEYWORD="jarvis"
+ 
+
 def speak(text:str):
     tts=pyttsx3.init()
     tts.say(text)
@@ -12,7 +41,21 @@ def speak(text:str):
     tts.stop()
     print("SPEAK IS CALLED")
 
+def open_web_domain(domain:str):
+    url=f"https://www.{domain}.com"
+    webbrowser.open_new(url)
 
+def processCommand(c):
+    if "open google" in c:
+        open_web_domain("google")
+    elif "open facebook" in c:
+        open_web_domain("facebook")
+    elif "open instagram" in c or "open insta" in c:
+        open_web_domain("instagram")
+    elif "open youtube" in c:
+        open_web_domain("youtube")
+    elif "news" in c:
+        handle_news()
 
 def websearch(query:str):
     url:str=f"https://www.google.com/search?q={query}"
@@ -52,7 +95,8 @@ def main():
             print("READY TO COMMAND")
             command:str=listenMicrophone(5)
             if command !="":
-                websearch(command)
+                # websearch(command)
+                processCommand(command)
                 print(f"Your command is{command}")
             
 
